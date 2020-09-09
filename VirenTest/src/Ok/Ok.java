7f69d65cfe71820c768bb42edfcd7e9f0e677f71
z0042y9h <z0042y9h@md2gdykc.ad001.siemens.net>
2020-09-09 11:44:50 +0200

    create new Projekt

---
+++ VirenTest/src/Ok/Ok.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ok;
+
+/**
+ *
+ * @author z0042y9h
+ */
+public class Ok {
+    
+}
