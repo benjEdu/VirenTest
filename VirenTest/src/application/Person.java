0632974743f4e9b5692c380fa84aee5829cce8e2
benni <benni@DESKTOP-4L4BK33>
2020-09-10 11:21:46 +0200

    person und mitarbeiter erstellt

--- VirenTest/src/application/Person.java
+++ VirenTest/src/application/Person.java
@@ -5,6 +5,91 @@
  */
 package application;
 
-public class Person {
+public abstract class Person {
+    private String nname;
+    private String vname;
+    private String email;
+    private String tel;
+    private String hr;
+    private String strasse;
+    private String stadt;
+    private String plz;
+    private String land;
+
+    public Person() {
+    }
+
+    public String getNname() {
+        return nname;
+    }
+
+    public void setNname(String nname) {
+        this.nname = nname;
+    }
+
+    public String getVname() {
+        return vname;
+    }
+
+    public void setVname(String vname) {
+        this.vname = vname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getHr() {
+        return hr;
+    }
+
+    public void setHr(String hr) {
+        this.hr = hr;
+    }
+
+    public String getStrasse() {
+        return strasse;
+    }
+
+    public void setStrasse(String strasse) {
+        this.strasse = strasse;
+    }
+
+    public String getStadt() {
+        return stadt;
+    }
+
+    public void setStadt(String stadt) {
+        this.stadt = stadt;
+    }
+
+    public String getPlz() {
+        return plz;
+    }
+
+    public void setPlz(String plz) {
+        this.plz = plz;
+    }
+
+    public String getLand() {
+        return land;
+    }
+
+    public void setLand(String land) {
+        this.land = land;
+    }
+    
     
 }
