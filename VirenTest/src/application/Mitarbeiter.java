9d7282719d7d6235c1713a4a6e6ba11944e4c572
benni <benni@DESKTOP-4L4BK33>
2020-09-10 11:21:33 +0200

    person und mitarbeiter erstellt

--- VirenTest/src/application/Mitarbeiter.java
+++ VirenTest/src/application/Mitarbeiter.java
@@ -9,6 +9,19 @@ package application;
  *
  * @author benni
  */
-public class Mitarbeiter {
+public abstract class Mitarbeiter extends Person{
+
+    private final String mitarbeiterId;
+    
+    public Mitarbeiter(String mitarbeiterId) {
+        super();
+        this.mitarbeiterId = mitarbeiterId;
+    }
+
+    public String getMitarbeiterId() {
+        return mitarbeiterId;
+    }
+    
+    
     
 }
