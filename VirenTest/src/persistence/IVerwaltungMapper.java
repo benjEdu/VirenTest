b6d5f9d0e63daa2884569b83cd524ac4ba1460c2
benni <benni@DESKTOP-4L4BK33>
2020-09-10 12:18:31 +0200

    Rest erstellt

---
+++ VirenTest/src/persistence/IVerwaltungMapper.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package persistence;
+
+import application.Testperson;
+import java.util.List;
+
+/**
+ *
+ * @author benni
+ */
+public interface IVerwaltungMapper {
+    public boolean einfuegenTestperson(Testperson p);
+    
+    public boolean aendernTestperson(Testperson p);
+    
+    public boolean loeschenTestperson(int id);
+    
+    Testperson lesenTestperson(int id);
+    
+    List<Testperson> lesenAlleTestpersonen();
+}
