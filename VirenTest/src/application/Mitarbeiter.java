bc3a7a7dd9691e881fee764f99519a1356e6ca4a
benni <benni@DESKTOP-4L4BK33>
2020-09-10 11:35:22 +0200

    Mitarbeiter erstellt

--- VirenTest/src/application/Mitarbeiter.java
+++ VirenTest/src/application/Mitarbeiter.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package application;
 
-/**
- *
- * @author benni
- */
+
 public abstract class Mitarbeiter extends Person{
 
     private final String mitarbeiterId;
@@ -21,7 +13,5 @@ public abstract class Mitarbeiter extends Person{
     public String getMitarbeiterId() {
         return mitarbeiterId;
     }
-    
-    
     
 }
