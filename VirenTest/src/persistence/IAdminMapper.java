ef81ef2ea93c1795fdc40135ddd14d48f9039567
benni <benni@DESKTOP-4L4BK33>
2020-09-10 12:18:35 +0200

    Rest erstellt

---
+++ VirenTest/src/persistence/IAdminMapper.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package persistence;
+
+import application.Mitarbeiter;
+import java.util.List;
+
+/**
+ *
+ * @author benni
+ */
+public interface IAdminMapper {
+    public boolean einfuegenMitarbeiter (Mitarbeiter m);
+    
+    public boolean aendernMitarbeiter (Mitarbeiter m);
+    
+    public boolean loeschenMitarbeiter (int id);
+    
+    Mitarbeiter lesenMitarbeiter (int id);
+    
+    List<Mitarbeiter> lesenAlleMitarbeiter ();
+}
