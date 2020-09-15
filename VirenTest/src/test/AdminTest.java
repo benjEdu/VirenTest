4624ebccf3ee07ac088afc6a728d4e99a12bfd76
benni <benni@DESKTOP-4L4BK33>
2020-09-15 09:14:02 +0200

    Alle Admin-Methoden und VerwaltungView

---
+++ VirenTest/src/test/AdminTest.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import view.MitarbeiterVerwaltungView;
+
+/**
+ *
+ * @author benni
+ */
+public class AdminTest {
+     public static void main(String[] args) {
+         new MitarbeiterVerwaltungView("Verwaltung");
+     }
+}
