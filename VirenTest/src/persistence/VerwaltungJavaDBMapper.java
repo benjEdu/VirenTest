c375a89ff6b645de8f0ac3ea6b827bc5fc4cc59d
z003sdxk <z003sdxk@md2ya1fc.ad001.siemens.net>
2020-09-16 16:06:41 +0200

    Neue Views Verwaltungstalle + TPAendern

--- VirenTest/src/persistence/VerwaltungJavaDBMapper.java
+++ VirenTest/src/persistence/VerwaltungJavaDBMapper.java
@@ -204,6 +204,7 @@ public class VerwaltungJavaDBMapper implements IVerwaltungMapper{
             PreparedStatement read = conn.prepareStatement("select * from testpersonen");
             ResultSet rs = read.executeQuery();
             while(rs.next()){
+                int tpId = rs.getInt("testpersonId");
                 String Vname = rs.getString("vname");
                 String Nname = rs.getString("nname");
                 String Email = rs.getString("email");
@@ -222,6 +223,7 @@ public class VerwaltungJavaDBMapper implements IVerwaltungMapper{
                     String plz = rs2.getString("plz");
                     String land = rs2.getString("land");
                     tp = new Testperson(adressId, Nname, Vname, Email, tel, hsNr, strasse, stadt, plz, land, salt, pwdHash);
+                    tp.setTestpersonId(tpId);
                 }else{
                     tp = new Testperson(Nname, Vname, Email, tel, salt, pwdHash);
                 }
--- VirenTest/src/view/TestpersonAendern.java
+++ VirenTest/src/view/TestpersonAendern.java
@@ -1,15 +1,13 @@
 package view;
 
-
 import application.Testperson;
 import application.VerwaltungVerwaltung;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+public class TestpersonAendern extends JFrame {
 
-public class TestpersonAendern extends JFrame{
-    
     //Hintergrundfarbe
     Color background = new Color(229, 255, 249);
     Color background2 = new Color(194, 255, 241);
@@ -17,8 +15,9 @@ public class TestpersonAendern extends JFrame{
     Font ueberschriftFont = new Font("SansSerif", Font.BOLD, 25);
     Font text = new Font("SansSerif", Font.BOLD, 17);
     private JLabel ueberschrift;
-    
-    private final VerwaltungVerwaltung vv;
+
+    private VerwaltungVerwaltung vv;
+
     
     private JLabel vNameLabel;
     private JLabel nNameLabel;
@@ -29,7 +28,8 @@ public class TestpersonAendern extends JFrame{
     private JLabel strasseLabel;
     private JLabel hnrLabel;
     private JLabel plzLabel;
-    
+    private int idTp;
+
     private JTextField vNameText;
     private JTextField nNameText;
     private JTextField emailText;
@@ -39,26 +39,27 @@ public class TestpersonAendern extends JFrame{
     private JTextField strasseText;
     private JTextField hnrText;
     private JTextField plzText;
-    
+
     private JButton aendern;
+    private JButton zurueck;
     
-    private Testperson te2;
-    
-    public TestpersonAendern(String titel, Testperson te){
+    private Testperson testperson;
+
+    public TestpersonAendern(String titel, Testperson tp) {
         super(titel);
         vv = new VerwaltungVerwaltung();
-        init(te);
+        init(tp);
     }
-    
-    private void init(Testperson te){
-        
-        te2 = te;
-        
+
+    private void init(Testperson tp) {
+
+        testperson = tp;
         //Überschrift
         ueberschrift = new JLabel("Willkommen beim Virustestcenter");
         ueberschrift.setHorizontalAlignment(JLabel.CENTER);
         ueberschrift.setFont(ueberschriftFont);
-        
+
+       
         vNameLabel = new JLabel("Vorname");
         nNameLabel = new JLabel("Nachname");
         emailLabel = new JLabel("Email");
@@ -68,92 +69,130 @@ public class TestpersonAendern extends JFrame{
         strasseLabel = new JLabel("Straße");
         hnrLabel = new JLabel("Hausnummer");
         plzLabel = new JLabel("Postleitzahl");
+        idTp = tp.getTestpersonId();
+
+        vNameText = new JTextField(tp.getVname());
+        nNameText = new JTextField(tp.getNname());
+        emailText = new JTextField(tp.getEmail());
+        telText = new JTextField(tp.getTel());
+        stadtText = new JTextField(tp.getStadt());
+        landText = new JTextField(tp.getLand());
+        strasseText = new JTextField(tp.getStrasse());
+        hnrText = new JTextField(tp.getHsNr());
+        plzText = new JTextField(tp.getPlz());
+
         
-        vNameText = new JTextField(te.getVname());
-        nNameText = new JTextField(te.getNname());
-        emailText = new JTextField(te.getEmail());
-        telText = new JTextField(te.getTel());
-        stadtText = new JTextField(te.getStadt());
-        landText = new JTextField(te.getLand());
-        strasseText = new JTextField(te.getStrasse());
-        hnrText = new JTextField(te.getHsNr());
-        plzText = new JTextField(te.getPlz());
+        vNameLabel.setFont(text);
+        nNameLabel.setFont(text);
+        emailLabel.setFont(text);
+        telLabel.setFont(text);
+        stadtLabel.setFont(text);
+        landLabel.setFont(text);
+        strasseLabel.setFont(text);
+        hnrLabel.setFont(text);
+        plzLabel.setFont(text);
         
-        setLayout(new GridLayout(10, 2));
+                
+        setLayout(new BorderLayout());
         
+        JPanel panel2 = new JPanel(new GridLayout(9, 2));
+        
+        JPanel panel3 = new JPanel(new FlowLayout());
+
         aendern = new JButton("Ändern");
+        zurueck = new JButton("Zurück");
+
+        aendern.setFont(text);
+        aendern.setBackground(background);
+        zurueck.setFont(text);
+        zurueck.setBackground(background);
+
         AendernButtonListener aendernListener = new AendernButtonListener();
         aendern.addActionListener(aendernListener);
+
+        MyActionListener2 listener2 = new MyActionListener2();
+        zurueck.addActionListener(listener2);
+
+        add(ueberschrift, BorderLayout.NORTH);
+
+        panel2.add(vNameLabel);
+        panel2.add(vNameText);
+        panel2.add(nNameLabel);
+        panel2.add(nNameText);
+        panel2.add(emailLabel);
+        panel2.add(emailText);
+        panel2.add(telLabel);
+        panel2.add(telText);
+        panel2.add(stadtLabel);
+        panel2.add(stadtText);
+        panel2.add(landLabel);
+        panel2.add(landText);
+        panel2.add(strasseLabel);
+        panel2.add(strasseText);
+        panel2.add(hnrLabel);
+        panel2.add(hnrText);
+        panel2.add(plzLabel);
+        panel2.add(plzText);
+        panel3.add(zurueck);
+        panel3.add(aendern);
+        add(panel2, BorderLayout.CENTER);
+        add(panel3, BorderLayout.SOUTH);
         
-        add(ueberschrift);
-        add(vNameLabel);
-        add(vNameText);
-        add(nNameLabel);
-        add(nNameText);
-        add(emailLabel);
-        add(emailText);
-        add(telLabel);
-        add(telText);
-        add(stadtLabel);
-        add(stadtText);
-        add(landLabel);
-        add(landText);
-        add(strasseLabel);
-        add(strasseText);
-        add(hnrLabel);
-        add(hnrText);
-        add(plzLabel);
-        add(plzText);
-        add(aendern);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(900,800);
+        setSize(900, 800);
         this.getContentPane().setBackground(background);
-        setLocation(400,400);
+        panel2.setBackground(background2);
+        panel3.setBackground(background2);
+        setLocation(400, 400);
         setVisible(true);
-        
+
     }
-    
+
     private class AendernButtonListener implements ActionListener {
+
         @Override
-            public void actionPerformed(ActionEvent e) {
-                    
-                    
-                    if(!vNameText.getText().isEmpty() && !vNameText.getText().equals("")){
-                        
-                        te2.setVname(vNameText.getText());
+        public void actionPerformed(ActionEvent e) {
+            Testperson tp = new Testperson(idTp);
+            if (vNameText.getText().trim() != null && !vNameText.getText().trim().isEmpty()) {
+                tp.setVname(vNameText.getText());
+                if (emailText.getText().trim() != null && !vNameText.getText().trim().isEmpty()) {
+                    tp.setEmail(emailText.getText());
+                    if (telText.getText().trim() != null && !vNameText.getText().trim().isEmpty()) {
+                        tp.setTel(telText.getText());
+                        if (stadtText.getText().trim() != null && !vNameText.getText().trim().isEmpty()) {
+                            tp.setStadt(stadtText.getText());
+                            if (landText.getText().trim() != null && !vNameText.getText().trim().isEmpty()) {
+                                tp.setLand(landText.getText());
+                                if (strasseText.getText().trim() != null && !vNameText.getText().trim().isEmpty()) {
+                                    tp.setStrasse(strasseText.getText());
+                                    if (hnrText.getText().trim() != null && !vNameText.getText().trim().isEmpty()) {
+                                        tp.setHr(hnrText.getText());
+                                        if (plzText.getText().trim() != null && !vNameText.getText().trim().isEmpty()) {
+                                            tp.setPlz(plzText.getText());
+                                            if (nNameText.getText().trim() != null && !nNameText.getText().trim().isEmpty()) {
+                                                tp.setNname(nNameText.getText());
+                                                vv.aendernTestperson(tp);
+                                                setVisible(false);
+                                                new TestpersonenVerwaltungstabelle("Virentestcenter");
+                                            }
+
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
-                    if(!nNameText.getText().isEmpty() && !nNameText.getText().equals("")){
-                        te2.setNname(nNameText.getText());
-                    }
-                    if(!emailText.getText().isEmpty() && !emailText.getText().equals("")){
-                        te2.setEmail(emailText.getText());
-                    }
-                    if(!telText.getText().isEmpty() && !telText.getText().equals("")){
-                        te2.setTel(telText.getText());
-                    }
-                    if(!stadtText.getText().isEmpty() && !stadtText.getText().equals("")){
-                        te2.setStadt(stadtText.getText());
-                    }
-                    if(!landText.getText().isEmpty() && !landText.getText().equals("")){
-                        te2.setLand(landText.getText());
-                    }
-                    if(!strasseText.getText().isEmpty() && !strasseText.getText().equals("")){
-                        te2.setStrasse(strasseText.getText());
-                    }
-                    if(!hnrText.getText().isEmpty() && !hnrText.getText().equals("")){
-                        te2.setHr(hnrText.getText());
-                    }
-                    if(!plzText.getText().isEmpty() && !plzText.getText().equals("")){
-                        te2.setPlz(plzText.getText());
-                    }
-                    System.out.println(te2.getVname());
-                    vv.aendernTestperson(te2);
-                    
-                    setVisible(false);
-                    new TestpersonenVerwaltungView("Verwaltung");
-                
-                
+                }
             }
+        }
+    }
+
+    private class MyActionListener2 implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            setVisible(false);
+            new TestpersonenVerwaltungstabelle("Virentestcenter");
+        }
     }
 }
-
