02b2ef61f8830572fcf44114a4ab62ff88ba7d76
z003sdxk <z003sdxk@md2ya1fc.ad001.siemens.net>
2020-09-16 13:52:57 +0200

    ZurückButton erstellt

--- VirenTest/src/view/MitarbeiterAendernView.java
+++ VirenTest/src/view/MitarbeiterAendernView.java
@@ -5,7 +5,6 @@
  */
 package view;
 
-
 import application.Admin;
 import application.AdminVerwaltung;
 import application.Laborant;
@@ -15,9 +14,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+public class MitarbeiterAendernView extends JFrame {
 
-public class MitarbeiterAendernView extends JFrame{
-    
     //Hintergrundfarbe
     Color background = new Color(229, 255, 249);
     Color background2 = new Color(194, 255, 241);
@@ -25,9 +23,9 @@ public class MitarbeiterAendernView extends JFrame{
     Font ueberschriftFont = new Font("SansSerif", Font.BOLD, 25);
     Font text = new Font("SansSerif", Font.BOLD, 17);
     private JLabel ueberschrift;
-    
+
     private AdminVerwaltung av;
-    
+
     private JLabel vNameLabel;
     private JLabel nNameLabel;
     private JLabel emailLabel;
@@ -39,8 +37,7 @@ public class MitarbeiterAendernView extends JFrame{
     private JLabel plzLabel;
     private JLabel bezeichnungLabel;
     private String idMit;
-    private JLabel infoLabel;
-    
+
     private JTextField vNameText;
     private JTextField nNameText;
     private JTextField emailText;
@@ -50,22 +47,23 @@ public class MitarbeiterAendernView extends JFrame{
     private JTextField strasseText;
     private JTextField hnrText;
     private JTextField plzText;
-    
+
     private JButton aendern;
-    
-    public MitarbeiterAendernView(String titel, Mitarbeiter m){
+    private JButton zurueck;
+
+    public MitarbeiterAendernView(String titel, Mitarbeiter m) {
         super(titel);
         av = new AdminVerwaltung();
         init(m);
     }
-    
-    private void init(Mitarbeiter m){
-        
+
+    private void init(Mitarbeiter m) {
+
         //Überschrift
         ueberschrift = new JLabel("Willkommen beim Virustestcenter");
         ueberschrift.setHorizontalAlignment(JLabel.CENTER);
         ueberschrift.setFont(ueberschriftFont);
-        
+
         vNameLabel = new JLabel("Vorname");
         nNameLabel = new JLabel("Nachname");
         emailLabel = new JLabel("Email");
@@ -75,10 +73,9 @@ public class MitarbeiterAendernView extends JFrame{
         strasseLabel = new JLabel("Straße");
         hnrLabel = new JLabel("Hausnummer");
         plzLabel = new JLabel("Postleitzahl");
-        infoLabel = new JLabel();
         bezeichnungLabel = new JLabel(m.getClass().getSimpleName());
         idMit = m.getMitarbeiterId();
-        
+
         vNameText = new JTextField(m.getVname());
         nNameText = new JTextField(m.getNname());
         emailText = new JTextField(m.getEmail());
@@ -88,93 +85,120 @@ public class MitarbeiterAendernView extends JFrame{
         strasseText = new JTextField(m.getStrasse());
         hnrText = new JTextField(m.getHsNr());
         plzText = new JTextField(m.getPlz());
+
+        setLayout(new BorderLayout());
         
-        setLayout(new GridLayout(12,2));
+        JPanel panel2 = new JPanel(new GridLayout(12, 2));
         
+        JPanel panel3 = new JPanel(new GridLayout(2, 2));
+
         aendern = new JButton("Ändern");
+        zurueck = new JButton("Zurück");
+
+        aendern.setFont(text);
+        aendern.setBackground(background);
+        zurueck.setFont(text);
+        zurueck.setBackground(background);
+
         AendernButtonListener aendernListener = new AendernButtonListener();
         aendern.addActionListener(aendernListener);
+
+        MyActionListener2 listener2 = new MyActionListener2();
+        zurueck.addActionListener(listener2);
+
+        add(ueberschrift, BorderLayout.NORTH);
+        panel2.add(bezeichnungLabel);
+        panel2.add(vNameLabel);
+        panel2.add(vNameText);
+        panel2.add(nNameLabel);
+        panel2.add(nNameText);
+        panel2.add(emailLabel);
+        panel2.add(emailText);
+        panel2.add(telLabel);
+        panel2.add(telText);
+        panel2.add(stadtLabel);
+        panel2.add(stadtText);
+        panel2.add(landLabel);
+        panel2.add(landText);
+        panel2.add(strasseLabel);
+        panel2.add(strasseText);
+        panel2.add(hnrLabel);
+        panel2.add(hnrText);
+        panel2.add(plzLabel);
+        panel2.add(plzText);
+        panel3.add(aendern);
+        panel3.add(zurueck);
+        add(panel2, BorderLayout.CENTER);
+        add(panel3, BorderLayout.SOUTH);
         
-        add(ueberschrift);
-        add(bezeichnungLabel);
-        add(vNameLabel);
-        add(vNameText);
-        add(nNameLabel);
-        add(nNameText);
-        add(emailLabel);
-        add(emailText);
-        add(telLabel);
-        add(telText);
-        add(stadtLabel);
-        add(stadtText);
-        add(landLabel);
-        add(landText);
-        add(strasseLabel);
-        add(strasseText);
-        add(hnrLabel);
-        add(hnrText);
-        add(plzLabel);
-        add(plzText);
-        add(aendern);
-        add(infoLabel);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(900,800);
+        setSize(900, 800);
         this.getContentPane().setBackground(background);
-        setLocation(400,400);
+        panel2.setBackground(background2);
+        setLocation(400, 400);
         setVisible(true);
-        
+
     }
-    
+
     private class AendernButtonListener implements ActionListener {
+
         @Override
-            public void actionPerformed(ActionEvent e) {
-                    Mitarbeiter mit = null;
-                    switch (bezeichnungLabel.getText()) {
-                        case "Admin":
-                            mit = new Admin(idMit);
-                            break;
-                        case "Verwaltung":
-                            mit = new Verwaltung(idMit);
-                            break;
-                        case "Laborant":
-                            mit = new Laborant(idMit);
-                            break;
-                        default:
-                            break;
-                    }
-                    
-                    if(vNameText.getText().trim() != null && !vNameText.getText().trim().isEmpty()){
-                        mit.setVname(vNameText.getText());
-                        if(emailText.getText().trim() != null && !vNameText.getText().trim().isEmpty()){
-                            mit.setEmail(emailText.getText());
-                            if(telText.getText().trim() != null && !vNameText.getText().trim().isEmpty()){
-                                mit.setTel(telText.getText());
-                                if(stadtText.getText().trim() != null && !vNameText.getText().trim().isEmpty()){
-                                    mit.setStadt(stadtText.getText());
-                                    if(landText.getText().trim() != null && !vNameText.getText().trim().isEmpty()){
-                                        mit.setLand(landText.getText());
-                                        if(strasseText.getText().trim() != null && !vNameText.getText().trim().isEmpty()){
-                                            mit.setStrasse(strasseText.getText());
-                                            if(hnrText.getText().trim() != null && !vNameText.getText().trim().isEmpty()){
-                                                mit.setHr(hnrText.getText());
-                                                if(plzText.getText().trim() != null && !vNameText.getText().trim().isEmpty()){
-                                                    mit.setPlz(plzText.getText());
-                                                    if(nNameText.getText().trim() != null && !nNameText.getText().trim().isEmpty()){
-                                                        mit.setNname(nNameText.getText());
-                                                        av.aendernMitarbeiter(mit);
-                                                        setVisible(false);
-                                                        new MitarbeiterVerwaltungView("Verwaltung");
-                                                    }
-                                                    
-                                                    
-                                                } 
+        public void actionPerformed(ActionEvent e) {
+            Mitarbeiter mit = null;
+            switch (bezeichnungLabel.getText()) {
+                case "Admin":
+                    mit = new Admin(idMit);
+                    break;
+                case "Verwaltung":
+                    mit = new Verwaltung(idMit);
+                    break;
+                case "Laborant":
+                    mit = new Laborant(idMit);
+                    break;
+                default:
+                    break;
+            }
+
+            if (vNameText.getText().trim() != null && !vNameText.getText().trim().isEmpty()) {
+                mit.setVname(vNameText.getText());
+                if (emailText.getText().trim() != null && !vNameText.getText().trim().isEmpty()) {
+                    mit.setEmail(emailText.getText());
+                    if (telText.getText().trim() != null && !vNameText.getText().trim().isEmpty()) {
+                        mit.setTel(telText.getText());
+                        if (stadtText.getText().trim() != null && !vNameText.getText().trim().isEmpty()) {
+                            mit.setStadt(stadtText.getText());
+                            if (landText.getText().trim() != null && !vNameText.getText().trim().isEmpty()) {
+                                mit.setLand(landText.getText());
+                                if (strasseText.getText().trim() != null && !vNameText.getText().trim().isEmpty()) {
+                                    mit.setStrasse(strasseText.getText());
+                                    if (hnrText.getText().trim() != null && !vNameText.getText().trim().isEmpty()) {
+                                        mit.setHr(hnrText.getText());
+                                        if (plzText.getText().trim() != null && !vNameText.getText().trim().isEmpty()) {
+                                            mit.setPlz(plzText.getText());
+                                            if (nNameText.getText().trim() != null && !nNameText.getText().trim().isEmpty()) {
+                                                mit.setNname(nNameText.getText());
+                                                av.aendernMitarbeiter(mit);
+                                                setVisible(false);
+                                                new MitarbeiterVerwaltungView("Verwaltung");
                                             }
+
                                         }
                                     }
                                 }
                             }
                         }
                     }
+                }
             }
+        }
+    }
+
+    private class MyActionListener2 implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            setVisible(false);
+            new MitarbeiterVerwaltungView("Virentestcenter");
+        }
     }
 }
