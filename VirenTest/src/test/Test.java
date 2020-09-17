376048e98be177c69205a7b06e84cd1f53a1ba5c
benni <benni@DESKTOP-4L4BK33>
2020-09-17 10:33:09 +0200

    ExceptionHandling

--- VirenTest/src/test/Test.java
+++ VirenTest/src/test/Test.java
@@ -1,24 +1,31 @@
 package test;
 
-import application.Login;
+import application.Laborant;
 import application.Mitarbeiter;
+import application.Person;
 import application.Testperson;
 import application.Verwaltung;
 import java.util.List;
 import view.TestergebnisLesen;
 import persistence.VerwaltungJavaDBMapper;
 import view.LoginView;
+import view.MitarbeiterAendernView;
 import view.MitarbeiterEinfuegen;
+import view.MitarbeiterVerwaltungView;
 import view.TestpersonEinfuegen;
 import view.TestpersonLoeschen;
 import view.TestpersonenVerwaltungView;
 import view.ViewLaborant;
+import view.TestpersonAendern;
+import view.TestpersonenVerwaltungstabelle;
 
 public class Test{
 
     public static void main(String[] args) {
-
-        System.out.println("Brombeere: "+Login.hashPassword("Brombeere", "c8a2ebb85d364556ba4d78cb9c9cd408", 0));
+        
+        /*
+        
+         System.out.println("Brombeere: "+Login.hashPassword("Brombeere", "c8a2ebb85d364556ba4d78cb9c9cd408", 0));
         System.out.println();
         System.out.println("Himbeere: "+Login.hashPassword("Himbeere", "1a7041115399402e9022c0eb03143ec5", 0));
         System.out.println();
@@ -34,11 +41,11 @@ public class Test{
         System.out.println();
         System.out.println("Pfirsich: "+Login.hashPassword("Pfirsich", "63c5db93e725465aaa57a33acba533b0", 0));
         LoginView lv = new LoginView("Login");
-        /*
         
-        VerwaltungJavaDBMapper mapper = new VerwaltungJavaDBMapper();
+        
+        
           
-        Testperson tp = new Testperson("Sentiten", "Dimitri", "test@testmail.com", "456498", "5", "laikestrasse", "Bonn", "9469", "Deutschelaaand");
+        
         Testperson tp2 = new Testperson("2Sentiten", "2Dimitri", "2test@testmail.com", "2456498", "25", "2laikestrasse", "2Bonn", "29469", "2Deutschelaaand");
         System.out.println(tp);
         System.out.println(tp2);
@@ -65,12 +72,12 @@ public class Test{
         //Testperson tp = new Testperson("Sentiten", "Dimitri", "test@testmail.com", "456498", "5", "laikestrasse", "Bonn", "9469", "Deutschelaaand");
         //System.out.println(tp);
         
-        //new TestergebnisLesen("Virentestcenter", 2);
+        //new TestergebnisLesen("Virentestcenter", 1);
         
         //new ViewLaborant("Virentestcenter");
         //new TestpersonEinfuegen("Virentestcenter");
         //new TestpersonLoeschen("Virentestcenter");
-        //new MitarbeiterEinfuegen("Virentestcenter");
+        new MitarbeiterEinfuegen("Virentestcenter");
         
         //mapper.einfuegenTestperson(tp);
         //List<Testperson> test = mapper.lesenAlleTestpersonen();
@@ -78,7 +85,61 @@ public class Test{
         //mapper.lesenTestperson(1);
         
         //new TestpersonenVerwaltungView("Virentestcenter");
+        //Testperson tp = new Testperson("Sentiten", "Dimitri", "test@testmail.com", "456498", "5", "laikestrasse", "Bonn", "9469", "Deutschland");
+        //Testperson tp2 = new Testperson(17);
+        //new TestpersonAendern("Virentestcenter", tp2);
+        
+        /*
+       Mitarbeiter tp = new Laborant("15");
+       tp.setEmail("test");
+       tp.setHr("1");
+       tp.setLand("DE");
+       tp.setNname("test");
+       tp.setPlz("test");
+       tp.setStadt("test");
+       tp.setStrasse("test");
+       tp.setTel("test");
+       tp.setVname("test");
+       
+       new MitarbeiterVerwaltungView("Virentestcenter");
+       //new MitarbeiterAendernView("Virentestcenter", tp);
+       */
+        
+        /*
+        Testperson tp2 = new Testperson(17);
+        tp2.setEmail("test");
+        tp2.setHr("1");
+        tp2.setLand("DE");
+        tp2.setNname("test");
+        tp2.setPlz("test");
+        tp2.setStadt("test");
+        tp2.setStrasse("test");
+        tp2.setTel("test");
+        tp2.setVname("test");
+        
+        Testperson tp3 = new Testperson(17);
+        tp3.setEmail("bla");
+        tp3.setHr("bla");
+        tp3.setLand("bla");
+        tp3.setNname("bla");
+        tp3.setPlz("bla");
+        tp3.setStadt("bla");
+        tp3.setStrasse("bla");
+        tp3.setTel("bla");
+        tp3.setVname("bla");
+        
+        VerwaltungJavaDBMapper mapper = new VerwaltungJavaDBMapper();
+        System.out.println(tp2.toString());
+        System.out.println(mapper.aendernTestperson(tp3));
+        mapper.aendernTestperson(tp3);
+        System.out.println(tp2.toString());
         
        
+                */
+        //new TestpersonenVerwaltungstabelle("Virentestcenter");
+        
+        //new MitarbeiterVerwaltungView("Virentestcenter");    
+        //new LoginView("Virentestcenter");
+    
     }
 }
