7d80732f3d6bef7090e020012d921e57d377262d
z003ub8r <z003ub8r@md1rg97c.ad001.siemens.net>
2020-09-18 10:16:57 +0200

    Blackboxtest für Salting und Hashing erweitert

--- VirenTest/src/test/HashSaltTest.java
+++ VirenTest/src/test/HashSaltTest.java
@@ -12,23 +12,32 @@ import application.Login;
 
 public class HashSaltTest {
     private static final String password = "ueberhauptnichtsicheresPasswort345";
-    private static final String salt = "ae62ef0054a04afc9f06aeede4341b30";
+    private static final String predefinedSalt = "ae62ef0054a04afc9f06aeede4341b30";
     private static final String correctHashedPassword = "116df06ac14f6d99256a5dfe602e388374d6628bc4893609e1c208fab865cc21dc1e28484600846921171abe3c7c9f6cf120e9f19de392a8070a48875ff20073";
     
     public static void main(String[] args){
-        if(testSaltGeneration()){
+        if(testSaltGeneration(predefinedSalt)){
             System.out.println("Generierung des Salts funktioniert :)");
+        }else{
+            System.out.println("Generierung des Salts funktioniert nicht :(");
+            System.out.println("Dadurch funktioniert auch automatisch das Hashing nicht.");
         }
-        if(testPasswordHashing(password, salt, correctHashedPassword)){
+        if(testPasswordHashing(password, predefinedSalt, correctHashedPassword)){
             System.out.println("Hashing des Passworts funktioniert :)");
+        }else{
+            System.out.println("Hashing des Passworts funktioniert nicht :(");
         }
+        
         
     }
     
-    public static boolean testSaltGeneration(){
+    public static boolean testSaltGeneration(String predefinedSalt){
         String salt = Login.getSalt();
         System.out.println("Generated Salt: " + salt);
         if(salt.length()==32){
+            if(salt.equals(predefinedSalt)){
+                return false;
+            }
             return true;
         }
         return false;
