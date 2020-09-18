ffa13baa8ba217bcea332e9d7a1b24857b87e7f6
z003ub8r <z003ub8r@md1rg97c.ad001.siemens.net>
2020-09-18 10:08:54 +0200

    Blackboxtest für Salting und Hashing

---
+++ VirenTest/src/test/HashSaltTest.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+import application.Login;
+/**
+ *
+ * @author z003ub8r
+ */
+
+public class HashSaltTest {
+    private static final String password = "ueberhauptnichtsicheresPasswort345";
+    private static final String salt = "ae62ef0054a04afc9f06aeede4341b30";
+    private static final String correctHashedPassword = "116df06ac14f6d99256a5dfe602e388374d6628bc4893609e1c208fab865cc21dc1e28484600846921171abe3c7c9f6cf120e9f19de392a8070a48875ff20073";
+    
+    public static void main(String[] args){
+        if(testSaltGeneration()){
+            System.out.println("Generierung des Salts funktioniert :)");
+        }
+        if(testPasswordHashing(password, salt, correctHashedPassword)){
+            System.out.println("Hashing des Passworts funktioniert :)");
+        }
+        
+    }
+    
+    public static boolean testSaltGeneration(){
+        String salt = Login.getSalt();
+        System.out.println("Generated Salt: " + salt);
+        if(salt.length()==32){
+            return true;
+        }
+        return false;
+    }
+    
+    public static boolean testPasswordHashing(String password, String salt, String correctHashedPassword){
+        String hashedPassword = Login.hashPassword(password, salt);
+        System.out.println("Hashed Password: " + hashedPassword);
+        if(hashedPassword.equals(correctHashedPassword)){
+            return true;
+        }
+        return false;
+    }
+}
